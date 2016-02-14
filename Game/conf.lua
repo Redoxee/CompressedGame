ccaea948be515b7c9ee62766eaffa8da68734520
Redoxee <anton.francis.roy@gmail.com>
2016-02-14 17:24:50 +0100

    added somes stuff in the conf file

--- Game/conf.lua
+++ Game/conf.lua
@@ -1,3 +1,7 @@
 function love.conf(t)
-    t.console = true                    -- Attach a console (boolean, Windows only)
+    t.identity = "CompressedGame"                   -- The name of the save directory (string)
+    t.version = "0.10.0"                -- The LÖVE version this game was made for (string)
+    t.console = false                  -- Attach a console (boolean, Windows only)
+    t.window.title = "CompressedGame"        -- The window title (string)
+    t.window.resizable = false         -- Let the window be user-resizable (boolean)
 end
